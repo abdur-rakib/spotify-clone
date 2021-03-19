a211e85c2bd963e114d7ca84a2425aa090396636
abdur-rakib <abdurrakib961@gmail.com>
2021-03-19 08:34:57 +0600

    Added basic login and dashboard page

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = ({ code }) => {
+  return (
+    <div>
+      <p>{code}</p>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ client/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div class="min-h-screen py-6 flex justify-center items-center">
+      <button className="bg-green-500 active:bg-green-700 text-white px-3 py-2 rounded font-semibold shadow-md focus:border-transparent">
+        Login with Spotify
+      </button>
+    </div>
+  );
+};
+
+export default Login;
